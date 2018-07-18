4c5bf758898ab6af665fb545c6542297cf1863f0
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 13:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function viewCart() {
     return 'Your shopping cart is empty.'
   } else if (cart.length === 2) {
     cartList.push(`${cart[0].itemName} at $${cart[0].itemPrice}`)
+    cartList.push(`${cart[1].itemName} at $${cart[1].itemPrice}`)
     return `In your cart, you have ${cartList.join('and ')}.`
     }
     
