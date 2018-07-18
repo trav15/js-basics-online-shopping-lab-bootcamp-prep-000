520076a68c994e4f25a59ef309872a919dc98e6d
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 13:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,22 @@ function viewCart() {
   var threeOrMore = 'In your cart, you have '
   if (!cart.length) { //empty cart
     return 'Your shopping cart is empty.'
+    
   } else if (cart.length === 2) { //two item cart
     cartList.push(`${cart[0].itemName} at $${cart[0].itemPrice}`)
     cartList.push(`${cart[1].itemName} at $${cart[1].itemPrice}`)
     return `In your cart, you have ${cartList.join(', and ')}.`
+    
   } else if (cart.length === 1) { //one item cart
     cartList.push(`${cart[0].itemName} at $${cart[0].itemPrice}`)
     return `In your cart, you have ${cartList.join(', and ')}.`    
+    
   } else { for (var i = 0; i < cart.length; i++) { //three or more items
     cartList.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
     if (i===cart.length - 1) {
-      threeOrMore += `, and ${cart[i].itemName} at $${cart[i].itemPrice}`
+      threeOrMore += `, and ${cart[i].itemName} at $${cart[i].itemPrice}.`
     } else {
-      threeOrMore += `${cart[i].itemName} at $${cart[i].itemPrice}`
+      threeOrMore += `${cart[i].itemName} at $${cart[i].itemPrice}, `
     }
   }
   return threeOrMore
