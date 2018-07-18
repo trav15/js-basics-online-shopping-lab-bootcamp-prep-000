0e99cf03e7d22215db40729a48e8a09f7ccb3e0d
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 14:08:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,12 +52,12 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++) {
+  for (var i = 0; i < cart.length) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
       return cart
     } else {
-      //do nothing and continue for loop
+      i++
     }
   return 'That item is not in your cart.'  
   }
