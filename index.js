dc873146f042d4876030cecef3b0fef54ae84769
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 13:09:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setCart(c) {
 function addToCart(item) {
  var itemObject = { itemName: item, itemPrice: `${Math.floor((Math.random() * 100) + 1)}`}
  cart.push(itemObject)
- return `${item} has been added to the cart`
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
