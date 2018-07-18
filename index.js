29ec15f9bcf73315de84c1adfe46b41842a8af28
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 14:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,8 +55,7 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
-      return
-      cart
+      return cart
     } else {
   }
   return 'That item is not in your cart.'  
@@ -70,4 +69,4 @@ function placeOrder(cardNumber) {
 addToCart('apples')
 addToCart('oranges')
 addToCart('flour')
-console.log(removeFromCart('oranges'))
+console.log(removeFromCart('apples'))
