2035f9c91847840bc211667d2fd34e012ad00f94
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 13:08:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var itemObject = { itemName: item, itemPrice: 
+ var itemObject = { itemName: item, itemPrice: `${Math.floor((Math.random() * 100) + 1)}`}
 }
 
 function viewCart() {
